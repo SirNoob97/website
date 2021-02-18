c85fa472d0a51f4d6341ee2c7649d3bd869407d0
Martin <martinald467@gmail.com>
2021-02-18 19:42:31 +0100

    add js file to decode the email

---
+++ www/js/email.js
@@ -0,0 +1,12 @@
+function decode(attValue){
+  return attValue.replace(/[a-zA-Z]/g, function(c){
+    return String.fromCharCode((c <= "Z" ? 90 : 122) >=  (c = c.charCodeAt(0) + 13) ? c : c - 26);
+  })
+};
+
+function openMailer(tag) {
+  var value = decode("email");
+  tag.setAttribute("href", value);
+  tag.setAttribute("onclick", "");
+  tag.firstChild.nodeValue = "I dont like spam bots";
+}
