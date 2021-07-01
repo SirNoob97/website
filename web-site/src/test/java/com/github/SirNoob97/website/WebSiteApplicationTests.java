e5a2cdb62ce483eefee320d91cba759f00c67497
SirNoob97 <martinald467@gmail.com>
2021-07-01 20:56:37 +0200

    add spring project

---
+++ web-site/src/test/java/com/github/SirNoob97/website/WebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.SirNoob97.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
