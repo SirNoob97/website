63eee15dd8e37657cf35a71bfd76bd76510864bf
SirNoob97 <martinald467@gmail.com>
2021-07-01 20:57:31 +0200

    add Web config, controller 'pending', and web resources

---
+++ web-site/src/main/java/com/github/SirNoob97/website/config/Web.java
@@ -0,0 +1,74 @@
+package com.github.SirNoob97.website.config;
+
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.pattern.PathPatternParser;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class Web implements ApplicationContextAware, WebMvcConfigurer {
+
+private ApplicationContext applicationContext;
+
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    this.applicationContext = applicationContext;
+  }
+
+  @Override
+  public void configurePathMatch(PathMatchConfigurer configurer) {
+    configurer.setPatternParser(new PathPatternParser()).addPathPrefix("/",
+        HandlerTypePredicate.forAnnotation(Controller.class));
+  }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/").setViewName("index.html");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("img/**").addResourceLocations("/img/", "classpath:/static/");
+    registry.addResourceHandler("/css/**").addResourceLocations("/css/", "classpath:/static/");
+    registry.addResourceHandler("js/**").addResourceLocations("/js/");
+  }
+
+  @Bean
+  public SpringResourceTemplateResolver templateResolver() {
+    var resolver = new SpringResourceTemplateResolver();
+    resolver.setApplicationContext(applicationContext);
+    resolver.setPrefix("classpath:/templates/");
+    resolver.setSuffix(".html");
+    resolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+    return resolver;
+  }
+
+  @Bean
+  public SpringTemplateEngine templateEngine() {
+    var engine = new SpringTemplateEngine();
+    engine.setTemplateResolver(templateResolver());
+    return engine;
+  }
+
+  @Bean
+  public ThymeleafViewResolver viewResolver() {
+    var resolver = new ThymeleafViewResolver();
+    resolver.setTemplateEngine(templateEngine());
+    return resolver;
+  }
+}
---
+++ web-site/src/main/java/com/github/SirNoob97/website/controller/WebSite.java
@@ -0,0 +1,10 @@
+package com.github.SirNoob97.website.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebSite {
+
+}
