ee77f209ff68d96ce2279a8c3d50994257718db1
SirNoob97 <martinald467@gmail.com>
2021-07-02 18:03:03 +0200

    reformat

--- web-site/src/main/java/com/github/SirNoob97/website/WebSiteApplication.java
+++ web-site/src/main/java/com/github/SirNoob97/website/WebSiteApplication.java
@@ -2,14 +2,10 @@ package com.github.SirNoob97.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan("com.github.SirNoob97.website")
+@SpringBootApplication(scanBasePackages = { "com.github.SirNoob97.website" })
 public class WebSiteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WebSiteApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(WebSiteApplication.class, args);
+  }
 }
