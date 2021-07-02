848929f8f056c28f3f20c269e4300e13f3210714
SirNoob97 <martinald467@gmail.com>
2021-07-02 18:34:42 +0200

    add index controller

--- web-site/src/main/java/com/github/SirNoob97/website/controller/WebSite.java
+++ web-site/src/main/java/com/github/SirNoob97/website/controller/WebSite.java
@@ -2,9 +2,12 @@ package com.github.SirNoob97.website.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebSite {
 
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
 }
